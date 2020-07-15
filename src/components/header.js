517bee7e9c173c937da4ad71375086d071ed9489
Xavier Boudreau <xavi.boudreau@gmail.com>
2020-07-15 15:39:52 -0400

    Add header logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,36 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import {Navbar,NavbarBrand,Nav,NavItem,Collapse} from "reactstrap"
 import styles from "./index/index.module.css"
+import logo from "../images/SLF-logo.png"
+
+/*<Link className = {styles.menuitem} to="/" style={{color: `black`,textDecoration: `none`}}>
+            {siteTitle}
+        </Link> */
+
+// <Navbar style = {{width: "100%", paddingTop: "0", paddingBottom: "0", margin: "0 auto"}}>
+//<NavItem styles = {{marginLeft: "1rem", marginRight: "1rem"}}>
+//<Nav className="mr-auto" styles = {{listStyle: "none", listStyleType: "none"}}>
 
 const Header = ({ siteTitle }) => (
-    <h1 style={{ margin: 0 }}>
-        <Link className = {styles.menuitem} to="/" style={{color: `black`,textDecoration: `none`}}>
-            {siteTitle}
-        </Link>
-    </h1>
+    <Navbar>
+        <NavbarBrand  href='/'>
+            <img src={logo} alt = "SLF logo" width="100" height="100"/>
+        </NavbarBrand>
+        <Collapse>
+            <Nav>
+                <NavItem>
+                        {siteTitle}
+                </NavItem>
+                <NavItem>
+                    <Link className = {styles.menuitem} to="/" style={{color: `black`,textDecoration: `none`}}>
+                        Menu 2
+                    </Link>
+                </NavItem>
+            </Nav>
+        </Collapse>
+    </Navbar>
 )
 
 Header.propTypes = {
